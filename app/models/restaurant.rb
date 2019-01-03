73c8cb28729f837f1d52a814549dee04758b9a5c
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-01-03 18:55:42 +0100

    no clue with the lecture

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  #dependent: :destroy lo usamos para que cuanod
+  #eliminemos un restaurant se elimine su review
+
+   validates :name, uniqueness: true, presence: true
+   validates :address, presence: true
+   validates :stars, inclusion: { in: [1, 2, 3, 4, 5] }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :restaurants, only: [:index]
+  end
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+    collection do
+      get 'top', to: "restaurants#top"
+    end
+    member do
+      get 'chef', to: "restaurants#chef"
+    end
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
+  get "restaurants/:id", to: "restaurants#show"
+  get "reviews/:id", to: "reviews#show"
+  get "reviews/:id/edit", to: "reviews#edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
